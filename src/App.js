72899c1c05c528bf968135ddac4ad85518222a17
Erich Kauffman <kauffm2@stolaf.edu>
2017-06-16 10:33:21 -0500

    Make http requests to the book inventory API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import BookMenu from './BookMenu';
 
 class App extends Component {
+
+  Count = () => {
+    let items = [];
+    let nums = [1,2,3,4,5,6,7,8,9,10];
+    nums.map( (i) => {
+      items.push(<li>{i}</li>);
+    });
+    return(
+      <ul>{items}</ul>
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Inventory</h2>
         </div>
+        {this.Count()}
         <BookMenu />
       </div>
     );
