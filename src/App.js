93efa59edf483f4652da128bb3fae18a9d5e8992
Erich Kauffman <kauffm2@stolaf.edu>
2017-06-18 15:33:51 -0500

    Add comments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import './App.css';
 import BookMenu from './BookMenu';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataReceived: false,
+      books: [],
+      bookIndex: null
+    };
+  }
+
+//Get data from book inventory API
+  getData = () => {
+    fetch('http://localhost:8000/all/')
+      .then( (response) => {
+        return response.json();
+      })
+      .then( (bookData) => {
+        this.setState({
+          dataReceived: true,
+          books: bookData
+        });
+      })
+      .catch( (err) => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
 
   render() {
     return (
@@ -10,7 +39,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Inventory</h2>
         </div>
-        <BookMenu />
+        <BookMenu bookData={this.state.books} />
       </div>
     );
   }
--- src/BookMenu.js
+++ src/BookMenu.js
@@ -2,45 +2,13 @@ import React, { Component } from 'react';
 import BookMenuItem from './BookMenuItem';
 
 export default class BookMenu extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      dataReceived: false,
-      books: []
-    };
-
-  }
-
-  getData = () => {
-    fetch('http://localhost:8000/all/short')
-      .then( (response) => {
-        return response.json();
-      })
-      .then( (bookData) => {
-        console.log(bookData);
-        this.setState({ //What if this is setting a state that is not the BookMenu?
-          dataReceived: true,
-          books: bookData
-        });
-      })
-      .catch( (err) => {
-        console.log(err);
-      });
-  }
 
   allBooks = () => {
-    if(this.state.dataReceived){
-      console.log("I am being called");
-      let bookItems = [];
-      this.state.books.map( (i) => {
-        bookItems.push(<BookMenuItem title={i.title} author={i.authors} id={i.rowid}/>);
-      });
-      return bookItems;
-    }
-  }
-
-  componentDidMount(){
-    this.getData();
+    let bookItems = [];
+    this.props.bookData.map( (i) => {
+      bookItems.push(<BookMenuItem title={i.title} author={i.authors} id={i.rowid}/>);
+    });
+    return bookItems;
   }
 
   render(){
