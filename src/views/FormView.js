c0a7640af9c963d6391644107d2f8cb26e1b1dcd
Erich Kauffman <kauffm2@stolaf.edu>
2017-08-13 11:04:05 -0500

    Saving changes

--- src/views/FormView.js
+++ src/views/FormView.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import TextButton from '../components/TextButton';
 
 export default class FormView extends Component{
   render(){
     return(
-      <p>Form View</p>
+      <div>
+        <label>
+          Title:
+          <input type='text' />
+        </label>
+        <label>
+          Author(s):
+          <input type='text' />
+        </label>
+        <label>
+          ISBN:
+          <input type='text' />
+        </label>
+        <label>
+          Edition:
+          <input type='text' />
+        </label>
+        <label>
+          Printing:
+          <input type='text' />
+        </label>
+        <TextButton>Submit</TextButton>
+      </div>
     )
   }
 }
