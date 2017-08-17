750c509ddbad5cd04c6b466e08caf5af02728ed3
Erich Kauffman <kauffm2@stolaf.edu>
2017-08-16 21:50:03 -0500

    Create handle click in TextButton
    Create on click handler for TextButton to exit the form view

--- src/components/TextButton.js
+++ src/components/TextButton.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 export default class TextButton extends Component{
+
+  handleClick = (e) => {
+    this.props.onClick(e.target.value);
+  }
+
   render(){
     return(
-      <div className="TextButton">
+      <div className="TextButton" onClick={this.props.onClick}>
         <p>{this.props.children}</p>
       </div>
     );
