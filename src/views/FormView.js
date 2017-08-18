2359a3fc2c0bfe7e24d757f4e04cb325ebefacb6
Erich Kauffman <kauffm2@stolaf.edu>
2017-08-18 02:06:19 -0500

    Create event handlers

--- src/views/FormView.js
+++ src/views/FormView.js
@@ -24,31 +24,100 @@ export default class FormView extends Component{
         authors: null,
         isbn: null,
         edition: null,
-        printing: null,
         yr_print: null,
-        date_purch: null,
+        printing: null,
+        date_purch: moment().format("MM/DD/YYYY"),
         condition: null,
         cover: null,
         loc_purch: null,
         amt_paid: null,
         sell_price: null,
-        site: null,
+        site: {
+          Ebay: false,
+          Amazon: false
+        },
         shelf: null
       }
     };
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = (date) => {
+  inputChange = (e) => {
+    let bookProps = this.state.bookProps;
+    bookProps[e.target.name] = e.target.value;
     this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  editionChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.edition = value;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  yearPrintChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.yr_print = value;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  printingChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.printing = value;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  dateChange = (date) => {
+    let bookProps = this.state.bookProps;
+    bookProps.date_purch = date.format("MM/DD/YYYY");
+    this.setState({
+      bookProps: bookProps,
       startDate: date
     });
   }
 
-  onChange = (value) => {
+  locationChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.loc_purch = value.value;
     this.setState({
-      locOps: value
+      bookProps: bookProps,
+      locVal: value
     });
+  }
+
+  amtPaidChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.amt_paid = value * 100;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  sellPriceChange = (value) => {
+    let bookProps = this.state.bookProps;
+    bookProps.sell_price = value * 100;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  siteChange = (e) => {
+    let bookProps = this.state.bookProps;
+    bookProps.site[e.target.value] = e.target.checked;
+    this.setState({
+      bookProps: bookProps
+    });
+  }
+
+  submitForm = () => {
+    console.log(this.state.bookProps);
+    this.props.changeView();
   }
 
   render(){
@@ -57,68 +126,68 @@ export default class FormView extends Component{
         <table className="formHolder">
           <tr>
             <th>Title:</th>
-            <td><input type="text" className="textIn"/></td>
+            <td><input type="text" className="textIn" name="title" onChange={this.inputChange}/></td>
           </tr>
           <tr>
             <th>Author(s):</th>
-            <td><input type="text" className="textIn"/></td>
+            <td><input type="text" className="textIn" name="authors" onChange={this.inputChange}/></td>
           </tr>
           <tr>
             <th>ISBN:</th>
-            <td><input type="text" className="textIn"/></td>
+            <td><input type="text" className="textIn" name="isbn" onChange={this.inputChange}/></td>
           </tr>
           <tr>
             <th>Edition:</th>
-            <td><NumericInput min={0} className="numberInputs"/></td>
+            <td><NumericInput min={0} className="numberInputs" onChange={this.editionChange}/></td>
           </tr>
           <tr>
             <th>Year Printed:</th>
-            <td><NumericInput className="numberInputs" min={0}/></td>
+            <td><NumericInput min={0} className="numberInputs" onChange={this.yearPrintChange}/></td>
           </tr>
           <tr>
             <th>Printing:</th>
-            <td><NumericInput className="numberInputs" min={0}/></td>
+            <td><NumericInput className="numberInputs" min={0} onChange={this.printingChange}/></td>
           </tr>
           <tr>
             <th>Cover Type:</th>
-            <td>Hard<input type="radio" name="cover" value="Hard"/>
-                Soft<input type="radio" name="cover" value="Soft"/></td>
+            <td>Hard<input type="radio" name="cover" value="Hard" onChange={this.inputChange}/>
+                Soft<input type="radio" name="cover" value="Soft" onChange={this.inputChange}/></td>
           </tr>
           <tr>
             <th>Condition:</th>
-            <td>New<input type="radio" name="condition" value="New"/>
-                Like New<input type="radio" name="condition" value="Like New"/>
-                Very Good<input type="radio" name="condition" value="Very Good"/>
-                Good<input type="radio" name="condition" value="Good"/>
-                Acceptable<input type="radio" name="condition" value="Acceptable"/></td>
+            <td>New<input type="radio" name="condition" value="New" onChange={this.inputChange}/>
+                Like New<input type="radio" name="condition" value="Like New" onChange={this.inputChange}/>
+                Very Good<input type="radio" name="condition" value="Very Good" onChange={this.inputChange}/>
+                Good<input type="radio" name="condition" value="Good" onChange={this.inputChange}/>
+                Acceptable<input type="radio" name="condition" value="Acceptable" onChange={this.inputChange}/></td>
           </tr>
           <tr>
             <th>Purchase Date:</th>
-            <td><DatePicker selected={this.state.startDate} onChange={this.handleChange} className="dateIn"/></td>
+            <td><DatePicker selected={this.state.startDate} onChange={this.dateChange} className="dateIn"/></td>
           </tr>
           <tr>
             <th>Purchase Location:</th>
-            <td><Select.Creatable className="purchase-location" name="purchase-location" options={locOps} value={this.state.bookProps.loc_purch} placeholder="select a location" onChange={this.onChange}/></td>
+            <td><Select.Creatable className="purchase-location" name="purchase-location" options={locOps} value={this.state.locVal} placeholder="select a location" onChange={this.locationChange}/></td>
           </tr>
           <tr>
             <th>Amount Paid:</th>
-            <td><NumericInput className="numberInputs" min={0} step={.01} precision={2}/></td>
+            <td><NumericInput className="numberInputs" min={0} step={.01} precision={2} onChange={this.amtPaidChange}/></td>
           </tr>
           <tr>
             <th>Sell Price:</th>
-            <td><NumericInput className="numberInputs" min={0} step={.01} precision={2}/></td>
+            <td><NumericInput className="numberInputs" min={0} step={.01} precision={2} onChange={this.sellPriceChange}/></td>
           </tr>
           <tr>
             <th>Site Listed:</th>
-            <td>Amazon<input type="checkbox" name="site" value="Amazon"/>
-                Ebay<input type="checkbox" name="site" value="Ebay"/></td>
+            <td>Amazon<input type="checkbox" name="site" value="Amazon" onChange={this.siteChange}/>
+                Ebay<input type="checkbox" name="site" value="Ebay" onChange={this.siteChange}/></td>
           </tr>
           <tr>
             <th>Shelf:</th>
-            <td><input type="text" className="textIn"/></td>
+            <td><input type="text" className="textIn" name="shelf" onChange={this.inputChange}/></td>
           </tr>
           <tr>
-            <th><TextButton onClick={this.props.changeView}>Submit</TextButton></th>
+            <th><TextButton onClick={this.submitForm}>Submit</TextButton></th>
             <td><TextButton onClick={this.props.changeView}>Back</TextButton></td>
 
           </tr>
