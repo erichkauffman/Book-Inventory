046b28edee8e5c0443556caf7a5ff8dc7820b598
Erich Kauffman <kauffm2@stolaf.edu>
2017-08-24 08:32:40 -0500

    Remove Nav.js

--- src/lib/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MainView from '../views/MainView';
-import FormView from '../views/FormView';
-
-//Do not export display
-export function chooseView(view, functionMain, functionForm){
-  let viewComponent = view === 'main' ? <MainView changeView={functionForm}/> : <FormView changeView={functionMain}/>;
-  return viewComponent;
-}
